bfff172b01221b2417deb42dade94df6e78a1442
Richard Du <richarddu@Richards-Mac.local>
2017-01-14 05:06:27 -0800

    Added push navigation

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -5,18 +5,16 @@
 //  Created by Richard Du on 1/6/17.
 //  Copyright © 2017 Richard Du. All rights reserved.
 //
-
 import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MoviesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
-    var filteredMovies: [NSDictionary]!
+    var endpoint: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +23,17 @@ class MoviesViewController: UIViewController, UICollectionViewDataSource, UIColl
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
         // add refresh control to table view
-        collectionView.insertSubview(refreshControl, at: 0)
+        tableView.insertSubview(refreshControl, at: 0)
         
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        searchBar.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         // Do any additional setup after loading the view.
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endpoint!)?api_key=\(apiKey)")
+        let request = URLRequest(url: url!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         
         // Display HUD right before the request is made
@@ -49,48 +48,56 @@ class MoviesViewController: UIViewController, UICollectionViewDataSource, UIColl
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     
                     self.movies = dataDictionary["results"] as! [NSDictionary]
-                    self.filteredMovies = self.movies
-                    self.collectionView.reloadData()
+                    self.tableView.reloadData()
                 }
             }
         }
-        
         task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let movies = filteredMovies {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let movies = movies {
             return movies.count
         } else {
             return 0
         }
+        
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
-        let movie = filteredMovies[indexPath.row]
-        let posterPath = movie["poster_path"] as! String
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as? String
+        let overview = movie["overview"] as? String
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
         
         let baseUrl = "https://image.tmdb.org/t/p/w500/"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
         
-        cell.posterView.setImageWith(imageUrl as! URL)
+        
+        if let posterPath = movie["poster_path"] as? String {
+        
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            cell.posterView.setImageWith(imageUrl as! URL)
+        }
         
         return cell
-
     }
     
     func refreshControlAction(refreshControl: UIRefreshControl) {
         
         // ... Create the NSURLRequest (myRequest) ...
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         // Configure session so that completion handler is executed on main UI thread
@@ -100,8 +107,7 @@ class MoviesViewController: UIViewController, UICollectionViewDataSource, UIColl
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     
                     self.movies = dataDictionary["results"] as! [NSDictionary]
-                    self.filteredMovies = self.movies
-                    self.collectionView.reloadData()
+                    self.tableView.reloadData()
                     // Tell the refreshControl to stop spinning
                     refreshControl.endRefreshing()
                 }
@@ -110,39 +116,21 @@ class MoviesViewController: UIViewController, UICollectionViewDataSource, UIColl
         task.resume()
     }
     
-    // This method updates filteredData based on the text in the Search Box
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        // When there is no text, filteredData is the same as the original data
-        // When user has entered text into the search box
-        // Use the filter method to iterate over all items in the data array
-        // For each item, return true if the item should be included and false if the
-        // item should NOT be included
-        filteredMovies = searchText.isEmpty ? movies : movies!.filter({(movieData: NSDictionary) -> Bool in
-            // If dataItem matches the searchText, return true to include it
-            
-            return (movieData["original_title"] as! String).range(of: searchText, options: .caseInsensitive) != nil
-        })
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies![indexPath!.row]
         
-        collectionView.reloadData()
-    }
-    
-    // called when keyboard search button pressed
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        view.endEditing(true)
-    }
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+     }
     
     
-    @IBAction func onTap(_ sender: AnyObject) {
-        view.endEditing(true)
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
